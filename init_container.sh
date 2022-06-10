1200ba5728025469279f5735e72bab934070b793
ZekeMarshall <marshall_zeke@outlook.com>
2022-06-10 12:39:12 +0100

    More alterations

--- init_container.sh
+++ init_container.sh
@@ -1,11 +1,9 @@
-#!/bin/sh
-set -e
+#!/usr/bin/env bash
+# Create the PrivSep empty directory if necessary
+if [ ! -d /run/sshd ]; then
+    mkdir /run/sshd
+    chmod 0755 /run/sshd
+fi
 
-# Get env vars in the Dockerfile to show up in the SSH session
-eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
-
-echo "Starting SSH ..."
-service ssh start
-
-echo "Running start-up command for the R Shiny App 'shiny::runApp('app.R', host = '0.0.0.0', port = 3838)'..."
-R -e "shiny::runApp('/app/app.R', host = '0.0.0.0', port = 3838)"
+# Start the SSH daemon in the foreground
+exec /usr/sbin/sshd -D 2>&1
