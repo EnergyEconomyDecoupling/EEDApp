fc7d69f462c09fbec735dce0d775eafcd833c3d1
ZekeMarshall <marshall_zeke@outlook.com>
2022-06-11 20:55:05 +0100

    Mounting PipelineReleases data

--- App-Modules/load_data.R
+++ App-Modules/load_data.R
@@ -1,11 +1,15 @@
-
 # -----
-mount_storage_data_cat <- drake::readd(target = PFUDatabase::target_names$completed_allocation_tables,
-                                       path = "/mnt/.drake",
-                                       character_only = TRUE)
+# mount_storage_data_cat <- drake::readd(target = PFUDatabase::target_names$completed_allocation_tables,
+#                                        path = "/mnt/.drake",
+#                                        character_only = TRUE)
+
+
+
 
 # PSUT_Agg_Re_all_St_pfu
-PSUT_Agg_Re_all_St_pfu <- read.csv(file = file.path("/pfu-output-data",
+PSUT_Agg_Re_all_St_pfu <- read.csv(file = file.path(#"Z:\\",
+                                                    "/pfu-output-data",
+                                                    "PipelineReleases",
                                                     "PSUT_Agg_Re_all_St_pfu",
                                                     "20220610T154839Z-219ee",
                                                     "PSUT_Agg_Re_all_St_pfu.csv"))
